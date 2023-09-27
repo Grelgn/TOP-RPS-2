207293066b344dd7278e25f824a97e7a361a691d
Grelgn <grelgn@hotmail.com>
2023-09-27 08:32:02 +0300

    Implements playRound
    Gets player selection and computer selection. Plays a round of the game
    and returns the result.

--- javascript.js
+++ javascript.js
@@ -8,3 +8,16 @@ function getComputerChoice() {
             return 'Scissors';
     }
 }
+
+function playRound(pSelection, cSelection) {
+    pSelection = pSelection[0].toUpperCase() + (pSelection.slice(1, pSelection.length)).toLowerCase();
+    if ((pSelection == 'Rock' && cSelection == 'Scissors') || (pSelection == 'Paper' && cSelection == 'Rock') || (pSelection == 'Scissors' && cSelection == 'Paper')) {
+        return `You Won! ${pSelection} beats ${cSelection}`;
+    }
+    else if (pSelection == cSelection) {
+        return `You Tied! ${pSelection} ties with ${cSelection}`;
+    }
+    else {
+        return `You Lost! ${cSelection} beats ${pSelection}`;
+    }
+}
