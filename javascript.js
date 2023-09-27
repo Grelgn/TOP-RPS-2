de2a1ca14ffa4e86ac9e54939c11718f3c9f45c4
Grelgn <grelgn@hotmail.com>
2023-09-27 09:48:49 +0300

    Implements 'game' function. Fixes invalid move.
    'game' function plays rounds of RPS until 5 rounds are won by either
    player. Displays total wins, ties, and losses.

--- javascript.js
+++ javascript.js
@@ -17,7 +17,47 @@ function playRound(pSelection, cSelection) {
     else if (pSelection == cSelection) {
         return `You Tied! ${pSelection} ties with ${cSelection}`;
     }
-    else {
+    else if ((pSelection == 'Rock' && cSelection == 'Paper') || (pSelection == 'Paper' && cSelection == 'Scissors') || (pSelection == 'Scissors' && cSelection == 'Rock')) {
         return `You Lost! ${cSelection} beats ${pSelection}`;
     }
+    else {
+        return 'INVALID MOVE'
+    }
 }
+
+function game() {
+    let winCount = 0;
+    let tieCount = 0;
+    let loseCount = 0;
+    while (1) {
+        let pSelection = prompt("Choose between 'Rock', 'Paper, or 'Scissors'");
+        let result = playRound(pSelection, getComputerChoice());
+        if (result.slice(4,7) == 'Won') {
+            winCount++;
+        }
+        else if (result.slice(4,8) == 'Tied') {
+            tieCount++;
+        }
+        else if (result.slice(4,8) == 'Lost') {
+            loseCount++;
+        }
+        
+        console.log(result);
+        console.log('Wins: ' + winCount);
+        console.log('Ties: ' + tieCount);
+        console.log('Losses: ' + loseCount);
+
+        if (winCount == 5) {
+            console.log('YOU WON THE GAME!')
+            break;
+        }
+        else if (loseCount == 5) {
+            console.log('YOU LOST THE GAME!')
+            break;
+        }
+    }
+}
+
+console.log('ROCK PAPER SCISSORS GAME!');
+console.log('FIRST TO WIN 5 ROUNDS WINS THE GAME!');
+game();
